31c403efb05b38bdfdf078d7c8752d3fdd25a756
GinaDiaz28 <gina.digina@gmail.com>
2021-10-14 23:07:50 -0500

    filtrado de libros añadido

--- e_librotek_app/views/getBook.py
+++ e_librotek_app/views/getBook.py
@@ -11,11 +11,61 @@ from e_librotek_app.serializers import BookSerializer
 class GetBook(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = (IsAuthenticated,)
-    def get(self, request, *args, **kwargs):
+    permission_classes = (AllowAny,)
+    
             
-        token = request.META.get('HTTP_AUTHORIZATION')[7:]
-        tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
-        valid_data = tokenBackend.decode(token,verify=False)
-        return Response(self.serializer_class.to_representation(self.serializer_class, obj=self.queryset.get(ISBN=request.data["ISBN"])), status.HTTP_200_OK)
-        return super().get(request, *args, **kwargs)
+    def get(self, request, *args, **kwargs):
+        valid_data = {}
+        try:
+            token = request.META.get('HTTP_AUTHORIZATION')[7:]
+            tokenBackend = TokenBackend(
+                algorithm=settings.SIMPLE_JWT['ALGORITHM'])
+            valid_data = tokenBackend.decode(token, verify=False)
+        except:
+            pass
+        filters = list(request.data["filters"])
+        query = {}
+        limit = 256
+        try:
+            limit = request.data["limit"]
+        except:
+            pass
+        for filter in filters:
+            if str(filter["option"]).lower() == "title":
+                query["title__icontains"] = filter["value"]
+            if "token_type" in valid_data.keys():
+                if str(filter["option"]).lower() == "isbn":
+                    query["ISBN"] = filter["value"]
+                elif str(filter["option"]).lower() == "gender":
+                    query["gender"] = filter["value"]
+                elif str(filter["option"]).lower() == "editorial":
+                    query["editorial__icontains"] = filter["value"]
+        books = self.queryset.filter(**query)
+        start = 0
+        end = len(books)
+        try:
+            start = request.data["offset"]["start"]
+            end = request.data["offset"]["end"]
+            if start >= len(books):
+                start = 0
+            if end > len(books):
+                end = len(books)
+            elif end <= 0:
+                end = len(books)+end
+            elif end <= start:
+                end = len(books)
+            else:
+                end += 1
+        except:
+            pass
+
+        res = {
+            "state": "success",
+            "result": []
+        }
+        for i in range(start, end):
+            if i <= limit:
+                serialized = self.serializer_class.to_representation(
+                    self.serializer_class, obj=books[i])
+                res["result"].append(serialized.copy())
+        return Response(res, status.HTTP_200_OK)

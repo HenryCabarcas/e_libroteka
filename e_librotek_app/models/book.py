dba19c437b932878988b390286e0892b4a405b48
nerdtrobik <cabarcasortiz@gmail.com>
2021-10-25 21:23:21 -0500

    Author field can be null

--- e_librotek_app/models/book.py
+++ e_librotek_app/models/book.py
@@ -6,7 +6,8 @@ class Book(models.Model):
     ISBN = models.CharField('ISBN', max_length=20,
                             unique=True, primary_key=True)
     title = models.CharField('title', max_length=50)
-    author = models.CharField('author', max_length=128, default="Anonymous")
+    author = models.CharField('author', max_length=128,
+                              default="Anonymous", null=True)
     gender = models.CharField('gender', max_length=40, null=True)
     publicationDate = models.CharField(
         'publicationDate', max_length=4, null=True)
@@ -16,4 +17,5 @@ class Book(models.Model):
     url = models.CharField('url', max_length=512, null=True)
     creationDate = models.DateTimeField(default=datetime.datetime.now())
     modificationDate = models.DateTimeField(default=datetime.datetime.now())
-    pages = models.BigIntegerField('pages', max_length=2000, default=0,null=True)
+    pages = models.BigIntegerField(
+        'pages', max_length=2000, default=0, null=True)

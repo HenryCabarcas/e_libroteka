3c3d7de13f5b20488842817e4889912c526465a9
nerdtrobik <cabarcasortiz@gmail.com>
2021-10-25 22:03:16 -0500

    increased value length limit

--- e_librotek_app/models/book.py
+++ e_librotek_app/models/book.py
@@ -5,10 +5,10 @@ import datetime
 class Book(models.Model):
     ISBN = models.CharField('ISBN', max_length=20,
                             unique=True, primary_key=True)
-    title = models.CharField('title', max_length=50)
+    title = models.CharField('title', max_length=128)
     author = models.CharField('author', max_length=128,
                               default="Anonymous", null=True)
-    gender = models.CharField('gender', max_length=40, null=True)
+    gender = models.CharField('gender', max_length=128, null=True)
     publicationDate = models.CharField(
         'publicationDate', max_length=4, null=True)
     formato = models.CharField(
@@ -18,4 +18,4 @@ class Book(models.Model):
     creationDate = models.DateTimeField(default=datetime.datetime.now())
     modificationDate = models.DateTimeField(default=datetime.datetime.now())
     pages = models.BigIntegerField(
-        'pages', max_length=2000, default=0, null=True)
+        'pages', default=0, null=True)
